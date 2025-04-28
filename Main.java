e6302359b406e1d83d1db4e6e065bfd4387f7db2
deanon-fresh <deanon.fresh@yandex.ru>
2025-04-28 16:01:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int Income = 44000;
+        int Spending = 30000;
+        System.out.println("Итого (руб):");
+        System.out.print(Income-Spending);
+
+
+    }
+}
